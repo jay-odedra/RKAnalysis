92e259bebcdb196250b61c2579c2c0666abb6936
crovelli <chiara.rovelli@cern.ch>
2021-08-25 09:42:16 +0200

    first commit

---
+++ macros/mcPlots.C
@@ -0,0 +1,284 @@
+#define mcPlots_cxx
+#include "mcPlots.h"
+#include <TH2.h>
+#include <TStyle.h>
+#include <TCanvas.h>
+#include <TLegend.h>   
+#include <iostream>  
+
+// To be run on MC tnp formatted ntuples to get distributions for 
+// - comparison with sPlots
+// - comparison between distribution pre/post tnp selection
+// Select signal and background based on match with MC-truth
+
+void mcPlots::Loop()
+{
+  if (fChain == 0) return;
+
+  // Analysis BDT distributions to final comparisons
+  TH1F *bdtOHs = new TH1F("bdtOHs","bdtOHs", 30, -15., 15.);
+  TH1F *bdtGHs = new TH1F("bdtGHs","bdtGHs", 30, -15., 15.);
+  bdtOHs->Sumw2();
+  bdtGHs->Sumw2();
+  TH1F *bdtOHb = new TH1F("bdtOHb","bdtOHb", 30, -15., 15.);
+  TH1F *bdtGHb = new TH1F("bdtGHb","bdtGHb", 30, -15., 15.);
+  bdtOHb->Sumw2();
+  bdtGHb->Sumw2();
+
+  // Other distributions
+  TH1F *tagPfIdHs   = new TH1F("tagPfIdHs",  "tagPfIdHs",  40,-10,10);
+  TH1F *probePfIdHs = new TH1F("probePfIdHs","probePfIdHs",40,-10,10);
+  TH1F *tagIdHs     = new TH1F("tagIdHs",    "tagIdHs",    40,-20,20);
+  TH1F *probeIdHs   = new TH1F("probeIdHs",  "probeIdHs",  40,-20,20);
+  tagPfIdHs->Sumw2();      
+  probePfIdHs->Sumw2();      
+  tagIdHs->Sumw2();      
+  probeIdHs->Sumw2();      
+  TH1F *tagPfIdHb   = new TH1F("tagPfIdHb",  "tagPfIdHb",  40,-10,10);
+  TH1F *probePfIdHb = new TH1F("probePfIdHb","probePfIdHb",40,-10,10);
+  TH1F *tagIdHb     = new TH1F("tagIdHb",    "tagIdHb",    40,-20,20);
+  TH1F *probeIdHb   = new TH1F("probeIdHb",  "probeIdHb",  40,-20,20);
+  tagPfIdHb->Sumw2();      
+  probePfIdHb->Sumw2();      
+  tagIdHb->Sumw2();      
+  probeIdHb->Sumw2();      
+  // 
+  TH1F *tagPtHs   = new TH1F("tagPtHs",  "tagPtHs",  20,0.,10.);
+  TH1F *probePtHs = new TH1F("probePtHs","probePtHs",20,0.,10.);
+  TH1F *kPtHs     = new TH1F("kPtHs",    "kPtHs",    20,0.,10.);
+  tagPtHs->Sumw2();
+  probePtHs->Sumw2();
+  kPtHs->Sumw2();
+  TH1F *tagPtHb   = new TH1F("tagPtHb",  "tagPtHb",  20,0.,10.);
+  TH1F *probePtHb = new TH1F("probePtHb","probePtHb",20,0.,10.);
+  TH1F *kPtHb     = new TH1F("kPtHb",    "kPtHb",    20,0.,10.);
+  tagPtHb->Sumw2();
+  probePtHb->Sumw2();
+  kPtHb->Sumw2();
+
+  // Loop over entries
+  Long64_t nentries = fChain->GetEntriesFast();
+  Long64_t nbytes = 0, nb = 0;
+  for (Long64_t jentry=0; jentry<nentries;jentry++) {
+    Long64_t ientry = LoadTree(jentry);
+    if (ientry < 0) break;
+    nb = fChain->GetEntry(jentry);   nbytes += nb;
+
+    // HLT applied in all cases at dumper level
+
+    // JPsi range 
+    if (pair_mass<2.9 || pair_mass>3.2) continue;
+
+    // Bmass range
+    if (B_mass<4.5 || B_mass>6) continue;
+
+    // Safety cuts
+    if (tagMvaId>20.5)     continue;
+    if (probeMvaId>20.5)   continue;
+    if (tagPfmvaId>20.5)   continue;
+    if (probePfmvaId>20.5) continue;
+    // 
+    if (theAnalysisBdtO>18)  continue;
+    if (theAnalysisBdtO<-18) continue;
+    if (theAnalysisBdtG>18)  continue;
+    if (theAnalysisBdtG<-18) continue;
+
+    // Fill distributions for real Bs
+    if (B_matchMC==1) {
+
+      bdtOHs -> Fill(theAnalysisBdtO);
+      bdtGHs -> Fill(theAnalysisBdtG);
+      
+      tagPtHs->Fill(tagPt);
+      probePtHs->Fill(probePt);
+      kPtHs->Fill(kPt);
+      
+      if (tagPfmvaId<20)   tagPfIdHs->Fill(tagPfmvaId);
+      if (probePfmvaId<20) probePfIdHs->Fill(probePfmvaId);
+      if (tagMvaId<20)     tagIdHs->Fill(tagMvaId);
+      if (probeMvaId<20)   probeIdHs->Fill(probeMvaId);
+
+    } else {
+
+      bdtOHb -> Fill(theAnalysisBdtO);
+      bdtGHb -> Fill(theAnalysisBdtG);
+      
+      tagPtHb->Fill(tagPt);
+      probePtHb->Fill(probePt);
+      kPtHb->Fill(kPt);
+      
+      if (tagPfmvaId<20)   tagPfIdHb->Fill(tagPfmvaId);
+      if (probePfmvaId<20) probePfIdHb->Fill(probePfmvaId);
+      if (tagMvaId<20)     tagIdHb->Fill(tagMvaId);
+      if (probeMvaId<20)   probeIdHb->Fill(probeMvaId);
+    }     
+
+  } // Loop over entries
+  
+  
+  // Cosmetics
+  bdtOHs->SetLineWidth(2);
+  bdtOHs->SetLineColor(6);
+  bdtGHs->SetLineWidth(2);
+  bdtGHs->SetLineColor(6);
+  bdtOHb->SetLineWidth(2);
+  bdtOHb->SetLineColor(4);
+  bdtGHb->SetLineWidth(2);
+  bdtGHb->SetLineColor(4);
+
+  tagPfIdHs->SetLineWidth(2);   
+  tagPfIdHs->SetLineColor(6);  
+  probePfIdHs->SetLineWidth(2);   
+  probePfIdHs->SetLineColor(6);  
+  tagIdHs->SetLineWidth(2);   
+  tagIdHs->SetLineColor(6); 
+  probeIdHs->SetLineWidth(2);   
+  probeIdHs->SetLineColor(6);  
+  tagPfIdHb->SetLineWidth(2);   
+  tagPfIdHb->SetLineColor(4);  
+  probePfIdHb->SetLineWidth(2);   
+  probePfIdHb->SetLineColor(4);  
+  tagIdHb->SetLineWidth(2);   
+  tagIdHb->SetLineColor(4); 
+  probeIdHb->SetLineWidth(2);   
+  probeIdHb->SetLineColor(4);  
+
+  tagPtHs->SetLineWidth(2);  
+  tagPtHs->SetLineColor(6);    
+  probePtHs->SetLineWidth(2);  
+  probePtHs->SetLineColor(6);    
+  kPtHs->SetLineWidth(2);  
+  kPtHs->SetLineColor(6);    
+  tagPtHb->SetLineWidth(2);  
+  tagPtHb->SetLineColor(4);    
+  probePtHb->SetLineWidth(2);  
+  probePtHb->SetLineColor(4);    
+  kPtHb->SetLineWidth(2);  
+  kPtHb->SetLineColor(4);    
+
+  // Save output
+  TFile myfile("myFileMC.root","RECREATE");
+  bdtOHs->Write();
+  bdtGHs->Write();
+  tagIdHs->Write();  
+  probeIdHs->Write();   
+  probePfIdHs->Write();   
+  tagPfIdHs->Write();    
+  tagPtHs->Write(); 
+  probePtHs->Write();  
+  kPtHs->Write(); 
+  myfile.Close();
+
+  // Plots
+  gROOT->SetStyle("Plain");
+  gStyle->SetOptStat(0);
+
+  TLegend *leg;
+  leg = new TLegend(0.10,0.65,0.45,0.90);
+  leg->SetFillStyle(0);
+  leg->SetBorderSize(0);
+  leg->SetTextSize(0.05);
+  leg->SetFillColor(0);
+  leg->AddEntry(bdtOHs, "Matching", "lp");
+  leg->AddEntry(bdtOHb, "Not matching", "lp");
+  //
+  TLegend *leg2;
+  leg2 = new TLegend(0.50,0.65,0.95,0.90);
+  leg2->SetFillStyle(0);
+  leg2->SetBorderSize(0);
+  leg2->SetTextSize(0.05);
+  leg2->SetFillColor(0);
+  leg2->AddEntry(bdtOHs, "Matching", "lp");
+  leg2->AddEntry(bdtOHb, "Not matching", "lp");
+
+
+  TCanvas c1("c1","c",1);
+  bdtOHs->SetTitle("");
+  bdtOHb->SetTitle("");
+  bdtOHs->GetXaxis()->SetTitle("BDT [Otto]");
+  bdtOHb->GetXaxis()->SetTitle("BDT [Otto]");
+  bdtOHs->DrawNormalized("hist");
+  bdtOHb->DrawNormalized("samehist");
+  leg->Draw();
+  c1.SaveAs("ottoBdt.png");
+
+  TCanvas c2("c2","c",1);
+  bdtGHs->SetTitle("");
+  bdtGHb->SetTitle("");
+  bdtGHs->GetXaxis()->SetTitle("BDT [George]");
+  bdtGHb->GetXaxis()->SetTitle("BDT [George]");
+  bdtGHs->DrawNormalized("hist");
+  bdtGHb->DrawNormalized("samehist");
+  leg->Draw();
+  c2.SaveAs("georgeBdt.png");
+
+  TCanvas c3("c3","c",1);
+  tagIdHs->SetTitle("");
+  tagIdHb->SetTitle("");
+  tagIdHs->GetXaxis()->SetTitle("tag eleID [LPT]");
+  tagIdHb->GetXaxis()->SetTitle("tag eleID [LPT]");
+  tagIdHb->DrawNormalized("hist");
+  tagIdHs->DrawNormalized("samehist");
+  leg->Draw();
+  c3.SaveAs("tagLptId.png");
+
+  TCanvas c4("c4","c",1);
+  probeIdHs->SetTitle("");
+  probeIdHb->SetTitle("");
+  probeIdHs->GetXaxis()->SetTitle("probe eleID [LPT]");
+  probeIdHb->GetXaxis()->SetTitle("probe eleID [LPT]");
+  probeIdHb->DrawNormalized("hist");
+  probeIdHs->DrawNormalized("samehist");
+  leg->Draw();
+  c4.SaveAs("probeLptId.png");
+
+  TCanvas c3a("c3a","c",1);
+  tagPfIdHs->SetTitle("");
+  tagPfIdHb->SetTitle("");
+  tagPfIdHs->GetXaxis()->SetTitle("tag eleID [PF]");
+  tagPfIdHb->GetXaxis()->SetTitle("tag eleID [PF]");
+  tagPfIdHb->DrawNormalized("hist");
+  tagPfIdHs->DrawNormalized("samehist");
+  leg->Draw();
+  c3a.SaveAs("tagPFId.png");
+
+  TCanvas c4a("c4a","c",1);
+  probePfIdHs->SetTitle("");
+  probePfIdHb->SetTitle("");
+  probePfIdHs->GetXaxis()->SetTitle("probe eleID [PF]");
+  probePfIdHb->GetXaxis()->SetTitle("probe eleID [PF]");
+  probePfIdHb->DrawNormalized("hist");
+  probePfIdHs->DrawNormalized("samehist");
+  leg->Draw();
+  c4a.SaveAs("probePfId.png");
+
+  TCanvas c5("c5","c",1);
+  tagPtHs->SetTitle("");
+  tagPtHb->SetTitle("");
+  tagPtHs->GetXaxis()->SetTitle("tag pt");
+  tagPtHb->GetXaxis()->SetTitle("tag pt");
+  tagPtHb->DrawNormalized("hist");
+  tagPtHs->DrawNormalized("samehist");
+  leg2->Draw();
+  c5.SaveAs("tagPt.png");
+
+  TCanvas c6("c6","c",1);
+  probePtHs->SetTitle("");
+  probePtHb->SetTitle("");
+  probePtHs->GetXaxis()->SetTitle("probe pt");
+  probePtHb->GetXaxis()->SetTitle("probe pt");
+  probePtHb->DrawNormalized("hist");
+  probePtHs->DrawNormalized("samehist");
+  leg2->Draw();
+  c6.SaveAs("probePt.png");
+
+  TCanvas c7("c7","c",1);
+  kPtHs->SetTitle("");
+  kPtHb->SetTitle("");
+  kPtHs->GetXaxis()->SetTitle("k pt");
+  kPtHb->GetXaxis()->SetTitle("k pt");
+  kPtHb->DrawNormalized("hist");
+  kPtHs->DrawNormalized("samehist");
+  leg2->Draw();
+  c7.SaveAs("kPt.png");
+}
---
+++ macros/mcPlots.h
@@ -0,0 +1,268 @@
+//////////////////////////////////////////////////////////
+// This class has been automatically generated on
+// Tue Aug 24 15:14:56 2021 by ROOT version 6.12/07
+// from TTree fitter_tree/reduced tree for T&P with B
+// found on file: /eos/cms/store/user/crovelli/LowPtEle/TnpDataB/March21noRegression/FormattedTnPForB_March21_BuToKJpsi_Toee_v2.root
+//////////////////////////////////////////////////////////
+
+#ifndef mcPlots_h
+#define mcPlots_h
+
+#include <TROOT.h>
+#include <TChain.h>
+#include <TFile.h>
+
+// Header file for the classes stored in the TTree if any.
+
+class mcPlots {
+public :
+   TTree          *fChain;   //!pointer to the analyzed TTree or TChain
+   Int_t           fCurrent; //!current Tree number in a TChain
+
+   // Fixed size dimensions of array or collections stored in the TTree if any.
+
+   // Declaration of leaf types
+   Int_t           theNvtx;
+   Float_t         tagPt;
+   Float_t         tagEta;
+   Float_t         tagPhi;
+   Float_t         tagPfmvaId;
+   Float_t         tagMvaId;
+   Float_t         tagDxySig;
+   Float_t         tagDzTrg;
+   Float_t         tagIso04Rel;
+   Float_t         probePt;
+   Float_t         probeEta;
+   Float_t         probePhi;
+   Float_t         probePfmvaId;
+   Float_t         probeMvaId;
+   Float_t         probeDxySig;
+   Float_t         probeDzTrg;
+   Float_t         probeIso04Rel;
+   Float_t         elesDr;
+   Float_t         kPt;
+   Float_t         kEta;
+   Float_t         kPhi;
+   Float_t         kDCASig;
+   Float_t         kDzTrg;
+   Float_t         kIso04Rel;
+   Float_t         kSvip2d;
+   Float_t         kSvip3d;
+   Float_t         B_mass;
+   Float_t         B_cos2d;
+   Float_t         B_pt;
+   Float_t         B_svprob;
+   Float_t         B_xysig;
+   Float_t         B_isoRel;
+   Int_t           B_matchMC;
+   Float_t         theLlkDR;
+   Float_t         thePtAsym;
+   Float_t         theLKdz;
+   Float_t         theL1L2dr;
+   Float_t         theLKdr;
+   Float_t         theKiso;
+   Float_t         theBBDPhi;
+   Float_t         theBTrkdxy2;
+   Float_t         theAnalysisBdtG;
+   Float_t         theAnalysisBdtO;
+   Float_t         pair_mass;
+   Float_t         weight;
+
+   // List of branches
+   TBranch        *b_theNvtx;   //!
+   TBranch        *b_tagPt;   //!
+   TBranch        *b_tagEta;   //!
+   TBranch        *b_tagPhi;   //!
+   TBranch        *b_tagPfmvaId;   //!
+   TBranch        *b_tagMvaId;   //!
+   TBranch        *b_tagDxySig;   //!
+   TBranch        *b_tagDzTrg;   //!
+   TBranch        *b_tagIso04Rel;   //!
+   TBranch        *b_probePt;   //!
+   TBranch        *b_probeEta;   //!
+   TBranch        *b_probePhi;   //!
+   TBranch        *b_probePfmvaId;   //!
+   TBranch        *b_probeMvaId;   //!
+   TBranch        *b_probeDxySig;   //!
+   TBranch        *b_probeDzTrg;   //!
+   TBranch        *b_probeIso04Rel;   //!
+   TBranch        *b_elesDr;   //!
+   TBranch        *b_kPt;   //!
+   TBranch        *b_kEta;   //!
+   TBranch        *b_kPhi;   //!
+   TBranch        *b_kDCASig;   //!
+   TBranch        *b_kDzTrg;   //!
+   TBranch        *b_kIso04Rel;   //!
+   TBranch        *b_kSvip2d;   //!
+   TBranch        *b_kSvip3d;   //!
+   TBranch        *b_B_mass;   //!
+   TBranch        *b_B_cos2d;   //!
+   TBranch        *b_B_pt;   //!
+   TBranch        *b_B_svprob;   //!
+   TBranch        *b_B_xysig;   //!
+   TBranch        *b_B_isoRel;   //!
+   TBranch        *b_B_matchMC;   //!
+   TBranch        *b_theLlkDR;   //!
+   TBranch        *b_thePtAsym;   //!
+   TBranch        *b_theLKdz;   //!
+   TBranch        *b_theL1L2dr;   //!
+   TBranch        *b_theLKdr;   //!
+   TBranch        *b_theKiso;   //!
+   TBranch        *b_theBBDPhi;   //!
+   TBranch        *b_theBTrkdxy2;   //!
+   TBranch        *b_theAnalysisBdtG;   //!
+   TBranch        *b_theAnalysisBdtO;   //!
+   TBranch        *b_pair_mass;   //!
+   TBranch        *b_weight;   //!
+
+   mcPlots(TTree *tree=0);
+   virtual ~mcPlots();
+   virtual Int_t    Cut(Long64_t entry);
+   virtual Int_t    GetEntry(Long64_t entry);
+   virtual Long64_t LoadTree(Long64_t entry);
+   virtual void     Init(TTree *tree);
+   virtual void     Loop();
+   virtual Bool_t   Notify();
+   virtual void     Show(Long64_t entry = -1);
+};
+
+#endif
+
+#ifdef mcPlots_cxx
+mcPlots::mcPlots(TTree *tree) : fChain(0) 
+{
+  // if parameter tree is not specified (or zero), connect the file
+  // used to generate this class and read the Tree.
+  if (tree == 0) {
+    TFile *f = (TFile*)gROOT->GetListOfFiles()->FindObject("/eos/cms/store/user/crovelli/LowPtEle/TnpDataB/March21noRegression/FormattedTnPForB_March21_BuToKJpsi_Toee_v2.root");
+    //TFile *f = (TFile*)gROOT->GetListOfFiles()->FindObject("../files_first/FormattedTnPForB_mcDumperNani_noSelApartHLT.root");
+    if (!f || !f->IsOpen()) {
+      f = new TFile("/eos/cms/store/user/crovelli/LowPtEle/TnpDataB/March21noRegression/FormattedTnPForB_March21_BuToKJpsi_Toee_v2.root");
+      //f = new TFile("../files_first/FormattedTnPForB_mcDumperNani_noSelApartHLT.root");
+    }
+    TDirectory * dir = (TDirectory*)f->Get("/eos/cms/store/user/crovelli/LowPtEle/TnpDataB/March21noRegression/FormattedTnPForB_March21_BuToKJpsi_Toee_v2.root:/tnpAna");
+    //TDirectory * dir = (TDirectory*)f->Get("../files_first/FormattedTnPForB_mcDumperNani_noSelApartHLT.root:/tnpAna");
+    dir->GetObject("fitter_tree",tree);
+    
+  }
+  Init(tree);
+}
+
+mcPlots::~mcPlots()
+{
+  if (!fChain) return;
+  delete fChain->GetCurrentFile();
+}
+
+Int_t mcPlots::GetEntry(Long64_t entry)
+{
+  // Read contents of entry.
+  if (!fChain) return 0;
+  return fChain->GetEntry(entry);
+}
+
+Long64_t mcPlots::LoadTree(Long64_t entry)
+{
+  // Set the environment to read one entry
+  if (!fChain) return -5;
+  Long64_t centry = fChain->LoadTree(entry);
+  if (centry < 0) return centry;
+  if (fChain->GetTreeNumber() != fCurrent) {
+    fCurrent = fChain->GetTreeNumber();
+    Notify();
+  }
+  return centry;
+}
+
+void mcPlots::Init(TTree *tree)
+{
+   // The Init() function is called when the selector needs to initialize
+   // a new tree or chain. Typically here the branch addresses and branch
+   // pointers of the tree will be set.
+   // It is normally not necessary to make changes to the generated
+   // code, but the routine can be extended by the user if needed.
+   // Init() will be called many times when running on PROOF
+   // (once per file to be processed).
+
+   // Set branch addresses and branch pointers
+   if (!tree) return;
+   fChain = tree;
+   fCurrent = -1;
+   fChain->SetMakeClass(1);
+
+   fChain->SetBranchAddress("theNvtx", &theNvtx, &b_theNvtx);
+   fChain->SetBranchAddress("tagPt", &tagPt, &b_tagPt);
+   fChain->SetBranchAddress("tagEta", &tagEta, &b_tagEta);
+   fChain->SetBranchAddress("tagPhi", &tagPhi, &b_tagPhi);
+   fChain->SetBranchAddress("tagPfmvaId", &tagPfmvaId, &b_tagPfmvaId);
+   fChain->SetBranchAddress("tagMvaId", &tagMvaId, &b_tagMvaId);
+   fChain->SetBranchAddress("tagDxySig", &tagDxySig, &b_tagDxySig);
+   fChain->SetBranchAddress("tagDzTrg", &tagDzTrg, &b_tagDzTrg);
+   fChain->SetBranchAddress("tagIso04Rel", &tagIso04Rel, &b_tagIso04Rel);
+   fChain->SetBranchAddress("probePt", &probePt, &b_probePt);
+   fChain->SetBranchAddress("probeEta", &probeEta, &b_probeEta);
+   fChain->SetBranchAddress("probePhi", &probePhi, &b_probePhi);
+   fChain->SetBranchAddress("probePfmvaId", &probePfmvaId, &b_probePfmvaId);
+   fChain->SetBranchAddress("probeMvaId", &probeMvaId, &b_probeMvaId);
+   fChain->SetBranchAddress("probeDxySig", &probeDxySig, &b_probeDxySig);
+   fChain->SetBranchAddress("probeDzTrg", &probeDzTrg, &b_probeDzTrg);
+   fChain->SetBranchAddress("probeIso04Rel", &probeIso04Rel, &b_probeIso04Rel);
+   fChain->SetBranchAddress("elesDr", &elesDr, &b_elesDr);
+   fChain->SetBranchAddress("kPt", &kPt, &b_kPt);
+   fChain->SetBranchAddress("kEta", &kEta, &b_kEta);
+   fChain->SetBranchAddress("kPhi", &kPhi, &b_kPhi);
+   fChain->SetBranchAddress("kDCASig", &kDCASig, &b_kDCASig);
+   fChain->SetBranchAddress("kDzTrg", &kDzTrg, &b_kDzTrg);
+   fChain->SetBranchAddress("kIso04Rel", &kIso04Rel, &b_kIso04Rel);
+   fChain->SetBranchAddress("kSvip2d", &kSvip2d, &b_kSvip2d);
+   fChain->SetBranchAddress("kSvip3d", &kSvip3d, &b_kSvip3d);
+   fChain->SetBranchAddress("B_mass", &B_mass, &b_B_mass);
+   fChain->SetBranchAddress("B_cos2d", &B_cos2d, &b_B_cos2d);
+   fChain->SetBranchAddress("B_pt", &B_pt, &b_B_pt);
+   fChain->SetBranchAddress("B_svprob", &B_svprob, &b_B_svprob);
+   fChain->SetBranchAddress("B_xysig", &B_xysig, &b_B_xysig);
+   fChain->SetBranchAddress("B_isoRel", &B_isoRel, &b_B_isoRel);
+   fChain->SetBranchAddress("B_matchMC", &B_matchMC, &b_B_matchMC);
+   fChain->SetBranchAddress("theLlkDR", &theLlkDR, &b_theLlkDR);
+   fChain->SetBranchAddress("thePtAsym", &thePtAsym, &b_thePtAsym);
+   fChain->SetBranchAddress("theLKdz", &theLKdz, &b_theLKdz);
+   fChain->SetBranchAddress("theL1L2dr", &theL1L2dr, &b_theL1L2dr);
+   fChain->SetBranchAddress("theLKdr", &theLKdr, &b_theLKdr);
+   fChain->SetBranchAddress("theKiso", &theKiso, &b_theKiso);
+   fChain->SetBranchAddress("theBBDPhi", &theBBDPhi, &b_theBBDPhi);
+   fChain->SetBranchAddress("theBTrkdxy2", &theBTrkdxy2, &b_theBTrkdxy2);
+   fChain->SetBranchAddress("theAnalysisBdtG", &theAnalysisBdtG, &b_theAnalysisBdtG);
+   fChain->SetBranchAddress("theAnalysisBdtO", &theAnalysisBdtO, &b_theAnalysisBdtO);
+   fChain->SetBranchAddress("pair_mass", &pair_mass, &b_pair_mass);
+   fChain->SetBranchAddress("weight", &weight, &b_weight);
+   Notify();
+}
+
+Bool_t mcPlots::Notify()
+{
+  // The Notify() function is called when a new file is opened. This
+  // can be either for a new TTree in a TChain or when when a new TTree
+  // is started when using PROOF. It is normally not necessary to make changes
+  // to the generated code, but the routine can be extended by the
+  // user if needed. The return value is currently not used.
+  
+  return kTRUE;
+}
+
+void mcPlots::Show(Long64_t entry)
+{
+  // Print contents of entry.
+  // If entry is not specified, print current entry
+  if (!fChain) return;
+  fChain->Show(entry);
+}
+
+Int_t mcPlots::Cut(Long64_t entry)
+{
+  // This function may be called from Loop.
+  // returns  1 if entry is accepted.
+  // returns -1 otherwise.
+  return 1;
+}
+
+#endif // #ifdef mcPlots_cxx
