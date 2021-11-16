4b2f7ecf3f7b855fcab8851847fc0ee166c2b056
crovelli <chiara.rovelli@cern.ch>
2021-11-16 11:46:25 +0100

    Common BDT and related variables/preselection implemented

--- include/SkimmerWithKStar.hh
+++ include/SkimmerWithKStar.hh
@@ -36,6 +36,7 @@ private:
   
   // Analysis methods
   bool isMcB( int myB );
+  bool isMcBOtto( int myB );
   bool isMcEleFromJPsi (int myEle );
   void bookOutputTree();
   void bookOutputHistos();
@@ -70,112 +71,104 @@ private:
   std::string _datasetName;      
 
   //---output tree branches variables
-  int    theRun;
-  int    theEvent;
-  int    nvtx;
-  int    theSampleID;
-  float  rho;
-  float  pu_weight;
-  float perEveW;
-  //
-  int hlt9;
+  int   theRun;
+  int   theEvent;
+  int   nvtx;
+  int   theSampleID;
+  float rho;
+  float pu_weight;
+  int   hlt7;
+  int   hlt9;
   float trg_muon_pt;
   // 
   int selectedBSize;
   //
   vector <float> p4Trk={};   
   vector <float> p4TrkKStar={};   
+  //
   vector <float> bXYOrder={};   
   vector <float> bPtOrder={};   
   vector <float> b2DOrder={};   
+  //
   vector <float> tag_pt={};   
   vector <float> tag_eta={};
   vector <float> tag_phi={};
   vector <bool> tag_isPF={};
   vector <bool> tag_isPFOverlap={};
   vector <bool> tag_isLowPt={};
-  vector <float> tag_mvaId={};
-  vector <float> tag_pfmvaId={};
+  vector <float> tag_id={};
+  vector <float> tag_drm={}; 
+  //
   vector <bool>  tag_matchMcFromJPsi={};
   vector <bool>  tag_matchMc={};
   vector <float> tag_ptMc={};
-  vector <float> probe_ptMc={};
-  vector <float> mll_fullfit={};
-  vector <float> mll_raw={};
-  vector <float> fit_mass={};
-
-  vector <float> fit_Bmass={};   
-  vector <float> fit_Bpt={};   
-  vector <float> fit_Bcos2D={};   
-  vector <float> fit_Bsvprob={};
-  vector <float> fit_Bxysig={};
-  vector <float>  fit_Bi3dsig={};
-  vector <bool> bmatchMC={};   
-  vector <float> Kpt={}; 
-  vector <float> K_eta={}; 
-  vector <float> K_phi={}; 
-  vector <float> k_dxy_sig_vec={}; 
-
+  //
   vector <float> probe_pt={}; 
-  vector <float> probe_drm={}; 
-  vector <float> tag_drm={}; 
   vector <float> probe_eta ={};
   vector <float> probe_phi={};
   vector <bool> probe_isPF ={};
   vector <bool> probe_isPFOverlap ={};
   vector <bool> probe_isLowPt ={};
-  vector <float> probe_mvaId={};        
-  vector <float> probe_pfmvaId={};        
-  vector <float> probe_dxySig={};        
-  vector <float> probe_dzSig={};        
-  vector <float> probe_unBiased ={};
-  vector <float> probe_ptBiased ={};
-
+  vector <float> probe_id={};        
+  vector <float> probe_drm={}; 
+  //
   vector <bool>  probe_matchMcFromJPsi={};
   vector <bool>  probe_matchMc={};
+  vector <float> probe_ptMc={};
+  //
+  vector <float> K_pt={}; 
+  vector <float> K_eta={}; 
+  vector <float> K_phi={}; 
+  //
+  vector <float> mll_fullfit={};
+  vector <float> mll_raw={};
+  //
+  vector <float> fit_Bmass={};   
+  vector <float> fit_Bpt={};   
+  vector <float> fit_Bcos2D={};   
+  vector <float> fit_Bsvprob={};
+  vector <float> fit_Bxysig={};
+  vector <float> fit_Bi3dsig={};
+  vector <bool> bmatchMC={};   
+  //
+  vector <float> LKdz_vec={};
+  vector <float> L1L2dr_vec={};
+  vector <float> L1L2dr_raw_vec={};
+  vector <float> b_iso04_rel_vec={};
+  vector <float> eleDR_vec={};
+  vector <float> k_DCASig_vec={};
+  vector <float> k_dzTrg_vec={};
+  vector <float> k_svip2d_vec={};
+  vector <float> k_svip3d_vec={};
+  vector <float> LKdr_vec={};
+  vector <float> LKdr_raw_vec={};
+  vector <float> BTrkdxy2_vec={};
+  vector <float> BBDPhi_vec={};
+  vector <float> BBDPhi_raw_vec={};
+  vector <float> ptAsym_vec={};
+  vector <float> ptAsym_raw_vec={};
+  vector <float> k_iso04_rel_vec={};
+  vector <float> tag_iso04_rel_vec={};
+  vector <float> probe_iso04_rel_vec={};
+  vector <float> k_dxy_sig_vec={}; 
+  vector <float> tag_dxy_sig_vec={};
+  vector <float> tag_dzTrg_vec={};
+  vector <float> probe_dxy_sig_vec={};
+  vector <float> probe_dzTrg_vec={};
+  vector <float> llkDR_vec={};
 
-  vector <bool> tag_convveto={};
-  vector <float> tag_pfRelIso={};
-
-  vector <bool> probe_convveto={};
-  vector <float> probe_pfRelIso={};
-  vector <float> probe_trkRelIso={};
-  vector <float> probe_fBrem={};
   vector <float> analysisBdtG={};
   vector <float> analysisBdtO={};
   vector <float> analysisBdtC={};
   vector <float> analysisBdtCWithSyst={};
 
-  vector <float> LKdz_vec={};
-  vector <float> L1L2dr_vec={};
-  vector <float> LKdr_vec={};
-  vector <float> Kiso_vec={};
-  vector <float> BBDPhi_vec={};
-  vector <float> BTrkdxy2_vec={};
-  vector <float> passymVar_vec={};
-
   vector <float> Dmass_vec={};
   vector <float> Dmass_flip_vec={};
   vector <float> Dmass_ll_flip_vec={};
   vector <float> Dmass_ll_vec={};
-
-  vector <float> b_iso04_rel_vec={};
-  vector <float> eleDR_vec={};
-  vector <float> k_DCASig_vec={};
-  vector <float> k_dzTrg_vec={};
-  vector <float> k_iso04_rel_vec={};
-  vector <float> k_svip2d_vec={};
-  vector <float> k_svip3d_vec={};
-  vector <float> tag_dxy_sig_vec={};
-  vector <float> tag_dzTrg_vec={};
-  vector <float> tag_iso04_rel_vec={};
-  vector <float> probe_dxy_sig_vec={};
-  vector <float> probe_dzTrg_vec={};
-  vector <float> probe_iso04_rel_vec={};
-  vector <float> llkDR_vec={};
-  vector <float> ptAsym_vec={};
-
-
+  vector <float> Dmass_mukhad_vec={};
+  vector <float> Dmass_mukmu_vec={};
+  vector <float> KLmassD0_vec={};
   //
   int selectedPairsSize;
 };
