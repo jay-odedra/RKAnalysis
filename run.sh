1c67b2bc8be46df06fab317079b21d8e17631ad5
bainbrid <bainbrid@users.noreply.github.com>
2021-12-17 23:55:53 +0100

    default is to run on full stats from latest prod

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
-#./Efficiency.exe ./data/file_list_BuToKee_v3.txt ./mc_BuToKee_v3 1 0 1000 -100. -100. -100.
+# .exe [file_list.txt] [outputFile] [1=MC,0=data] [nmin] [nmax] [ptB] [svprob] [co2D]
 
-./Efficiency.exe ./data/file_list_temp.txt ./mc_BuToKee_temp 1 0 1000 -100. -100. -100.
+#./Efficiency.exe ./data/file_list_temp.txt ./mc_BuToKee_temp 1 0 1000 -100. -100. -100.
+./Efficiency.exe ./data/file_list_BuToKee_v3_2021Dec16.txt ./mc_BuToKee_2021Dec16 1 0 1000 -100. -100. -100.
 
 #./SkimmerWithKStar.exe file_list_BuToKee_v2.txt ./mc_BuToKee_v2_0_1 1 0 1 0. 0. -1. >& mc_BuToKee_v2_0_1.log &
-
-# .exe [file_list.txt] [outputFile] [1=MC,0=data] [nmin] [nmax] [ptB] [svprob] [co2D]
